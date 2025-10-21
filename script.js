8188d04728889fe627adfd22ccecc9de3e0f4d4e
esfat7-hub <freelancer.mahmudul.75@gmail.com>
2025-10-21 22:12:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Team data & ranking logic
 const teams = [
   {name:'Arabian Kings', matches:6, wins:4, losses:2, points:8, nrr:+1.223},
-  {name:'Knight Riders', matches:3, wins:2, losses:1, points:4, nrr:+1.378},
-  {name:'Flying Falcon', matches:4, wins:2, losses:2, points:4, nrr:+0.450},
+  {name:'Knight Riders', matches:4, wins:3, losses:1, points:6, nrr:+3.708},
+  {name:'Flying Falcon', matches:5, wins:2, losses:3, points:4, nrr:-1.88},
   {name:'Power Heaters', matches:4, wins:2, losses:2, points:4, nrr:-0.309},
   {name:'All-round Royals', matches:3, wins:0, losses:3, points:0, nrr:-2.665}
 ];
